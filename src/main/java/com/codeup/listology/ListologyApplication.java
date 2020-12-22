579005eadf43c3906d5b20b1cb47968c4bc68885
Michael Klanica <michael.david.klanica@gmail.com>
2020-12-22 12:23:49 -0600

    initial commit

---
+++ src/main/java/com/codeup/listology/ListologyApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.listology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListologyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListologyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/listology/ListologyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.listology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListologyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
