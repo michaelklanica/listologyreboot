377d5a535d84c9c8f715da21c86213444c78d343
Michael Klanica <michael.david.klanica@gmail.com>
2020-12-22 15:41:40 -0600

    added constructors and getter and setters to models

--- src/main/java/com/codeup/listology/models/Post.java
+++ src/main/java/com/codeup/listology/models/Post.java
@@ -5,6 +5,7 @@ import javax.persistence.*;
 @Entity
 @Table(name = "posts")
 public class Post {
+//    PROPERTIES:
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -14,4 +15,45 @@ public class Post {
 
     @Column(nullable = false, columnDefinition = "TEXT")
     private String body;
+
+//    CONSTRUCTORS:
+    public Post() {}
+
+    public Post(String title, String body) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+
+    public Post(long id, String title, String body) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+
+//    GETTERS and SETTERS:
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
 }
--- src/main/java/com/codeup/listology/models/User.java
+++ src/main/java/com/codeup/listology/models/User.java
@@ -5,6 +5,7 @@ import javax.persistence.*;
 @Entity
 @Table(name = "users")
 public class User {
+//    PROPERTIES:
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -15,6 +16,42 @@ public class User {
     @Column(nullable = false, length = 100)
     private String password;
 
+//    CONSTRUCTORS:
+    public User() {}
 
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 
+    public User(long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+//    GETTERS and SETTERS:
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
